f92a2053b47b137099ef1689411f0a46b44aa5aa
DiegoCastr00 <dcastroe2100@alumno.ipn.mx>
2024-08-18 00:19:12 -0600

    fixing enviroment

--- src/components/const/companies.ts
+++ src/components/const/companies.ts
@@ -3,7 +3,7 @@ export const companies = [
         id: "latgoblab",
         name: "Lat Gob Lab",
         description:
-            "....",
+            "...",
         img: "amazon.jpg",
         books: [
             "Factores que influyen la supervivencia de las MIPYMES",
